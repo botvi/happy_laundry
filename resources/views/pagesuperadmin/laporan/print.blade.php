b603e684536174480948d0bde62b597969dcf134
Yovi Ardiansyah <younoob07mei@gmail.com>
2026-06-09 22:59:59 +0700

    first commit

--- resources/views/pagesuperadmin/laporan/print.blade.php
+++ resources/views/pagesuperadmin/laporan/print.blade.php
@@ -140,7 +140,7 @@
                      <td>{{ $loop->iteration }}</td>
                      <td>
                          @if(($p->pelanggan->user->role ?? '') == 'superadmin')
-                             Offline ({{ $p->nama_pelanggan ?? 'Walk-in' }})
+                            ({{ $p->nama_pelanggan ?? 'Walk-in' }})
                          @else
                              {{ $p->pelanggan->user->name ?? '-' }}
                          @endif
